68fe39fb4b2ae124088fe3f2cf515430153545ea
Sarah <hayes.sc@gmail.com>
2017-06-20 19:03:03 -0400

    Updated js file and added about.html file

--- js/lmo.js
+++ js/lmo.js
@@ -1,3 +1,19 @@
-$( "button" ).click(function() {
+/*$( "button" ).click(function() {
   $( "#search" ).show();
+});
+*/
+var searchButton = document.getElementById('search-button');
+var searchBar = document.getElementById('search-bar');
+
+
+searchButton.addEventListener('click', () => {
+    if (searchBar.classList.contains('hideme')) {
+    searchBar.classList.remove('hideme');
+  } else {
+    searchBar.classList.add('hideme');
+  }
+  if (searchBar.value !== '') {
+   alert("Sorry! Search function has not been implemented.");
+   searchBar.value = '';
+ }
 });
