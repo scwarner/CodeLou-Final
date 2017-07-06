f91185934bc2f38c08f4cbbeab2534a418779cb7
Sarah <hayes.sc@gmail.com>
2017-07-05 21:30:08 -0400

    Added user interaction to contact form via JS and improved footer

--- js/lmo.js
+++ js/lmo.js
@@ -1,19 +1,37 @@
-/*$( "button" ).click(function() {
-  $( "#search" ).show();
-});
-*/
+
 var searchButton = document.getElementById('search-button');
 var searchBar = document.getElementById('search-bar');
+var joinCheckBox = document.getElementById('join');
+var joinField = document.getElementById('join-field');
+var giveCheckBox = document.getElementById('give');
+var giveField = document.getElementById('donation-field');
+var hireCheckBox = document.getElementById('hire');
+var hireField = document.getElementById('hire-field')
 
-
+//Reveals search bar when search button is clicked
 searchButton.addEventListener('click', () => {
     if (searchBar.classList.contains('hideme')) {
     searchBar.classList.remove('hideme');
   } else {
     searchBar.classList.add('hideme');
   }
+  //If user enters text in search bar, reveals modal window with message
   if (searchBar.value !== '') {
    alert("Sorry! Search function has not been implemented.");
    searchBar.value = '';
  }
+});
+
+//Reveals form questions regarding joining LMO when clicked
+joinCheckBox.addEventListener('click', () => {
+  joinField.style.display = 'block';
+});
+
+//Reveals donation information when user clicks checkbox
+giveCheckBox.addEventListener('click', () => {
+  giveField.style.display = 'block';
+});
+
+hireCheckBox.addEventListener('click', () => {
+  hireField.style.display = 'block';
 });
