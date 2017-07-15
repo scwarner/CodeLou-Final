51d9f010531f1be0c4045674f08c8d36bf0e494e
Sarah <hayes.sc@gmail.com>
2017-07-15 07:28:13 -0400

    Added jquery to make buttons on concerts page functional

--- js/lmo.js
+++ js/lmo.js
@@ -8,6 +8,10 @@ var giveField = document.getElementById('donation-field');
 var hireCheckBox = document.getElementById('hire');
 var hireField = document.getElementById('hire-field')
 
+/***********
+ALL PAGES
+***********/
+
 //Reveals search bar when search button is clicked
 searchButton.addEventListener('click', function () {
     if (searchBar.classList.contains('hideme')) {
@@ -15,12 +19,34 @@ searchButton.addEventListener('click', function () {
   } else {
     searchBar.classList.add('hideme');
   }
-  //If user enters text in search bar, reveals modal window with message
+  //If user enters text in search bar and then clicks search button, reveals alert with message
   if (searchBar.value !== '') {
    alert("Sorry! Search function has not been implemented.");
    searchBar.value = '';
  }
 });
+
+/************
+CONCERTS PAGE
+************/
+
+//When 'Free Shows Only' button is clicked, filters table to show rows with free concerts
+$('.free-shows').on('click', function () {
+    $('.ticket').toggleClass('hideme');
+    $('tbody tr:nth-child(even)').toggleClass('clicked');
+    //$('tbody tr:nth-child(even)').css('background-color', 'white');
+    //$('tbody tr:nth-child(even)').css('color', '#000');
+})
+
+//When 'Local Shows Only' button is clicked, filters table to show rows with local concerts
+$('.local-shows').on('click', function () {
+  $('.away').toggleClass('hideme');
+})
+
+
+/************
+CONTACT PAGE
+************/
 
 //Reveals form questions regarding joining LMO when clicked
 joinCheckBox.addEventListener('click', function () {
